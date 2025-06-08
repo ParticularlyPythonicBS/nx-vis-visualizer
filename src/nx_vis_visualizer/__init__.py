7db43cb69d6b21681710bab82f519810a55d5e27
ParticularlyPythonicBS <anil@anilr.dev>
2025-06-07 21:28:00 -0400

    versioning consistency and bump

--- src/nx_vis_visualizer/__init__.py
+++ src/nx_vis_visualizer/__init__.py
@@ -1,8 +1,10 @@
 # src/nx_vis_visualizer/__init__.py
 
+from importlib.metadata import version
+
 from .core import DEFAULT_VIS_OPTIONS, nx_to_vis
 
-__version__ = "0.1.1"  # Should match pyproject.toml
+__version__ = version("nx-vis-visualizer")  # Get version from pyproject.toml
 
 __all__ = [
     "DEFAULT_VIS_OPTIONS",
